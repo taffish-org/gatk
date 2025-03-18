b73af9acc3487f1f0224822a612b74e2b2d18bc6
Kaiyuan Han <hermit_han@163.com>
2025-03-18 17:27:17 +0800

    add functions

--- gatk.info.lisp
+++ gatk.info.lisp
@@ -4,4 +4,12 @@
 (:version        "4.6.1.0")
 (:intro          "Variant Discovery in High-Throughput Sequencing Data")
 (:dependencies)
-(:functions      "gatk")
+(:functions      "gatk"
+                 "bedtools :: v2.30.0"
+                 "samtools :: v1.13"
+                 "bcftools :: v1.13"
+                 "tabix    :: v1.13"
+                 "python   :: v3.10.13"
+                 "python   :: [numpy] [scipy] [pytorch] [pymc3] [keras] [scikit-learn] [matplotlib] [pandas] [biopython] [pyvcf] [pysam]"
+                 "R        :: v4.3.1"
+                 "R        :: [data.table] [dplyr] [ggplot2]")
